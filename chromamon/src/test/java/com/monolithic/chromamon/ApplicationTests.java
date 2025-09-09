755f0b103d9934fcf56c184b517fd4677f7dd545
Paulo-Possatto <paulo.possatto@outlook.com>
2025-09-09 20:40:19 +0200

    Added basic tests and resources

--- chromamon/src/test/java/com/monolithic/chromamon/ApplicationTests.java
+++ chromamon/src/test/java/com/monolithic/chromamon/ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.monolithic.chromamon;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -9,5 +11,7 @@ import org.springframework.context.annotation.Import;
 class ApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertTrue(Boolean.TRUE);
+  }
 }
